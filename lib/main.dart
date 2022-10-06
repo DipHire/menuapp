d858e3bf0cc4fb1a49e04460dc50bd0d4e9e96d9
Vaibhav Prajapati <vaibhavprajapati0101@gmail.com>
2022-10-07 00:05:22 +0530

    Added Login Page Orange Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:menuapp/pages/homepage.dart';
+import 'package:menuapp/pages/loginpage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      routes: {
+        "/homepage": (context) => const HomePage(),
+      },
     );
   }
 }
